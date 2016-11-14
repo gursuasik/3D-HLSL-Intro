5ee72c8b10aa3afd44fcaa52b83647cc22244e29
gursuasik <gursuasik@gmail.com>
2016-11-14 14:04:14 +0300

    3D HLSL Intro

---
+++ README.md
@@ -0,0 +1,7 @@
+#3D HLSL Intro
+
+<img border="0" src="https://3.bp.blogspot.com/-mFRZ8I7srqQ/UVIfMKPdp1I/AAAAAAAAAH0/pfIKLHNcv80/s320/G%25C3%25BCrsu+A%25C5%259F%25C4%25B1k+3D+HLSL+Intro.jpg">
+
+Araba farına sabit şekilli ışık oluşturularak, önünde bulunan cisimlerin arkalarında gölgelendirme oluşturuldu.
+
+https://gursuasik.blogspot.com.tr/2013/03/directx-3d-game-in-c-15-yllk-calsmann.html
---
+++ Series3D3/Series3D3/Game1.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace Series3D3
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public struct MyOwnVertexFormat
+    {
+        public Vector3 position;
+        private Vector2 texCoord;
+        private Vector3 normal;
+        public MyOwnVertexFormat(Vector3 position, Vector2 texCoord, Vector3 normal)
+        {
+            this.position = position;
+            this.texCoord = texCoord;
+            this.normal = normal;
+        }
+        public readonly static VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+            new VertexElement(sizeof(float) * 3, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+            new VertexElement(sizeof(float) * (3 + 2), VertexElementFormat.Vector3, VertexElementUsage.Normal, 0)
+        );
+    }
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        GraphicsDevice device;
+        Effect effect;
+        Matrix viewMatrix;
+        Matrix projectionMatrix;
+        VertexBuffer vertexBuffer;
+        Vector3 cameraPos;
+        Texture2D streetTexture;
+        Model lamppostModel;
+        Texture2D[] lamppostTextures;
+        Model carModel;
+        Texture2D[] carTextures;
+        Vector3 lightPos;
+        float lightPower;
+        float ambientPower;
+        Matrix lightsViewProjectionMatrix;
+        RenderTarget2D renderTarget;
+        Texture2D shadowMap;
+        Texture2D carLight;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            Window.Title = "Riemer's XNA Tutorials -- Series 3";
+            base.Initialize();
+        }
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            device = GraphicsDevice;
+            effect = Content.Load<Effect>("OurHLSLfile"); SetUpVertices();
+            SetUpCamera();
+            streetTexture = Content.Load<Texture2D>("streettexture"); 
+            carModel = LoadModel("car", out carTextures);
+            lamppostModel = LoadModel("lamppost", out lamppostTextures);
+            lamppostTextures[0] = streetTexture;
+            PresentationParameters pp = device.PresentationParameters;
+            renderTarget = new RenderTarget2D(device, pp.BackBufferWidth, pp.BackBufferHeight, true, device.DisplayMode.Format, DepthFormat.Depth24);
+            carLight = Content.Load<Texture2D>("carlight");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            UpdateLightData();
+            // TODO: Add your update logic here
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            device.SetRenderTarget(renderTarget);
+            device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Black, 1.0f, 0);
+            DrawScene("ShadowMap");
+            device.SetRenderTarget(null);
+            shadowMap = (Texture2D)renderTarget;
+            device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Black, 1.0f, 0);
+            DrawScene("ShadowedScene");
+            shadowMap = null;
+            // TODO: Add your drawing code here
+            base.Draw(gameTime);
+        }
+        private void SetUpVertices()
+        {
+            MyOwnVertexFormat[] vertices = new MyOwnVertexFormat[18];
+            vertices[0] = new MyOwnVertexFormat(new Vector3(-20, 0, 10), new Vector2(-0.25f, 25.0f), new Vector3(0, 1, 0));
+            vertices[1] = new MyOwnVertexFormat(new Vector3(-20, 0, -100), new Vector2(-0.25f, 0.0f), new Vector3(0, 1, 0));
+            vertices[2] = new MyOwnVertexFormat(new Vector3(2, 0, 10), new Vector2(0.25f, 25.0f), new Vector3(0, 1, 0));
+            vertices[3] = new MyOwnVertexFormat(new Vector3(2, 0, -100), new Vector2(0.25f, 0.0f), new Vector3(0, 1, 0));
+            vertices[4] = new MyOwnVertexFormat(new Vector3(2, 0, 10), new Vector2(0.25f, 25.0f), new Vector3(-1, 0, 0));
+            vertices[5] = new MyOwnVertexFormat(new Vector3(2, 0, -100), new Vector2(0.25f, 0.0f), new Vector3(-1, 0, 0));
+            vertices[6] = new MyOwnVertexFormat(new Vector3(2, 1, 10), new Vector2(0.375f, 25.0f), new Vector3(-1, 0, 0));
+            vertices[7] = new MyOwnVertexFormat(new Vector3(2, 1, -100), new Vector2(0.375f, 0.0f), new Vector3(-1, 0, 0));
+            vertices[8] = new MyOwnVertexFormat(new Vector3(2, 1, 10), new Vector2(0.375f, 25.0f), new Vector3(0, 1, 0));
+            vertices[9] = new MyOwnVertexFormat(new Vector3(2, 1, -100), new Vector2(0.375f, 0.0f), new Vector3(0, 1, 0));
+            vertices[10] = new MyOwnVertexFormat(new Vector3(3, 1, 10), new Vector2(0.5f, 25.0f), new Vector3(0, 1, 0));
+            vertices[11] = new MyOwnVertexFormat(new Vector3(3, 1, -100), new Vector2(0.5f, 0.0f), new Vector3(0, 1, 0));
+            vertices[12] = new MyOwnVertexFormat(new Vector3(13, 1, 10), new Vector2(0.75f, 25.0f), new Vector3(0, 1, 0));
+            vertices[13] = new MyOwnVertexFormat(new Vector3(13, 1, -100), new Vector2(0.75f, 0.0f), new Vector3(0, 1, 0));
+            vertices[14] = new MyOwnVertexFormat(new Vector3(13, 1, 10), new Vector2(0.75f, 25.0f), new Vector3(-1, 0, 0));
+            vertices[15] = new MyOwnVertexFormat(new Vector3(13, 1, -100), new Vector2(0.75f, 0.0f), new Vector3(-1, 0, 0));
+            vertices[16] = new MyOwnVertexFormat(new Vector3(13, 21, 10), new Vector2(1.25f, 25.0f), new Vector3(-1, 0, 0));
+            vertices[17] = new MyOwnVertexFormat(new Vector3(13, 21, -100), new Vector2(1.25f, 0.0f), new Vector3(-1, 0, 0));
+            vertexBuffer = new VertexBuffer(device, MyOwnVertexFormat.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+            vertexBuffer.SetData(vertices);
+        }
+        private void SetUpCamera()
+        {
+            cameraPos = new Vector3(-25, 13, 18);
+            viewMatrix = Matrix.CreateLookAt(cameraPos, new Vector3(0, 2, -12), new Vector3(0, 1, 0));
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, device.Viewport.AspectRatio, 1.0f, 200.0f);
+        }
+        private Model LoadModel(string assetName, out Texture2D[] textures)
+        {
+            Model newModel = Content.Load<Model>(assetName);
+            textures = new Texture2D[7];
+            int i = 0;
+            foreach (ModelMesh mesh in newModel.Meshes)
+                foreach (BasicEffect currentEffect in mesh.Effects)
+                    textures[i++] = currentEffect.Texture;
+            foreach (ModelMesh mesh in newModel.Meshes)
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                    meshPart.Effect = effect.Clone();
+            return newModel;
+        }
+        private void DrawModel(Model model, Texture2D[] textures, Matrix wMatrix, string technique)
+        {
+            Matrix[] modelTransforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+            int i = 0;
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (Effect currentEffect in mesh.Effects)
+                {
+                    Matrix worldMatrix = modelTransforms[mesh.ParentBone.Index] * wMatrix;
+                    currentEffect.CurrentTechnique = currentEffect.Techniques[technique];
+                    currentEffect.Parameters["xCamerasViewProjection"].SetValue(viewMatrix * projectionMatrix);
+                    currentEffect.Parameters["xLightsViewProjection"].SetValue(lightsViewProjectionMatrix);
+                    currentEffect.Parameters["xWorld"].SetValue(worldMatrix);
+                    currentEffect.Parameters["xTexture"].SetValue(textures[i++]);
+                    currentEffect.Parameters["xLightPos"].SetValue(lightPos);
+                    currentEffect.Parameters["xLightPower"].SetValue(lightPower);
+                    currentEffect.Parameters["xAmbient"].SetValue(ambientPower);
+                    currentEffect.Parameters["xShadowMap"].SetValue(shadowMap);
+                    currentEffect.Parameters["xCarLightTexture"].SetValue(carLight);
+                }
+                mesh.Draw();
+            }
+        }
+        private void UpdateLightData()
+        {
+            ambientPower = 0.2f;
+            lightPos = new Vector3(-18, 5, -2);
+            lightPower = 2f; 
+            Matrix lightsView = Matrix.CreateLookAt(lightPos, new Vector3(-2, 3, -10), new Vector3(0, 1, 0));
+            Matrix lightsProjection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1f, 5f, 100f);
+            lightsViewProjectionMatrix = lightsView * lightsProjection;
+        }
+        private void DrawScene(string technique)
+        {
+            effect.CurrentTechnique = effect.Techniques[technique];
+            effect.Parameters["xCamerasViewProjection"].SetValue(viewMatrix * projectionMatrix);
+            effect.Parameters["xLightsViewProjection"].SetValue(lightsViewProjectionMatrix);
+            effect.Parameters["xWorld"].SetValue(Matrix.Identity);
+            effect.Parameters["xTexture"].SetValue(streetTexture);
+            effect.Parameters["xLightPos"].SetValue(lightPos);
+            effect.Parameters["xLightPower"].SetValue(lightPower);
+            effect.Parameters["xAmbient"].SetValue(ambientPower);
+            effect.Parameters["xShadowMap"].SetValue(shadowMap);
+            effect.Parameters["xCarLightTexture"].SetValue(carLight);
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.SetVertexBuffer(vertexBuffer);
+                device.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 16);
+            }
+            Matrix car1Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi) * Matrix.CreateTranslation(-3, 0, -15);
+            DrawModel(carModel, carTextures, car1Matrix, technique);
+            Matrix car2Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi * 5.0f / 8.0f) * Matrix.CreateTranslation(-28, 0, -1.9f);
+            DrawModel(carModel, carTextures, car2Matrix, technique);
+            Matrix car3Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi * 0) * Matrix.CreateTranslation(-3, 0, 5f);
+            DrawModel(carModel, carTextures, car3Matrix, technique);
+        }
+    }
+}
